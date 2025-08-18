159f77260a77b6c3bfd346cc2077a5dc51cb6e9b
RogerJackD <rogerzapata114@gmail.com>
2025-08-17 23:56:10 -0500

    implement guard example

---
+++ src/user/guards/use-role-admin.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class UseRoleAdminGuard implements CanActivate {
+
+    constructor(
+        private readonly reflector: Reflector,
+    ){}
+    
+    canActivate(
+        context: ExecutionContext
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        
+        const metaDataFound = this.reflector.get<string[]>('hello', context.getHandler());
+
+        console.log(metaDataFound)
+        
+        console.log(context);
+
+        return true
+    }
+}
