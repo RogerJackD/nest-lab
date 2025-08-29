1b6d5d972c19874052d763453a945e4cd9d18adb
RogerJackD <rogerzapata114@gmail.com>
2025-08-28 23:26:06 -0500

    feat(products): add create dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,25 @@
-export class CreateProductDto {}
+import { IsArray, IsNumber, IsOptional, IsPositive, IsString, MinLength } from 'class-validator';
+
+export class CreateProductDto {
+
+    @IsString()
+    @MinLength(2)
+    title: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price: number;
+
+    @IsString()
+    @IsOptional()
+    description: string;
+
+    @IsString()
+    main_image: string;
+
+    @IsString({each: true})
+    @IsArray()
+    @IsOptional()
+    gallery_images: string[];
+}
